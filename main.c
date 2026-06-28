37ef01f9a210bc20c27bdb3f2d47c326489d37e1
vishu <vishnuparihar.delhi5@gmail.com>
2026-06-28 07:51:59 +0530

    Drew Strip

--- main.c
+++ main.c
@@ -5,6 +5,24 @@
 #define WIDTH 900
 #define HEIGHT 600
 
+float segment_width = 200;
+float segment_thickness = 50;
+
+void drawStrip(Vector2 center) {
+  int count = 6;
+
+  Vector2 a, b, c, d, e, f;
+  a = (Vector2){center.x - segment_width / 2 - segment_thickness / 2, center.y};
+  b = (Vector2){center.x - segment_width / 2, center.y - segment_thickness / 2};
+  c = (Vector2){center.x - segment_width / 2, center.y + segment_thickness / 2};
+  d = (Vector2){center.x + segment_width / 2, center.y - segment_thickness / 2};
+  e = (Vector2){center.x + segment_width / 2, center.y + segment_thickness / 2};
+  f = (Vector2){center.x + segment_width / 2 + segment_thickness / 2, center.y};
+
+  Vector2 points[] = {a, c, b, e, d, f};
+  DrawTriangleStrip(points, count, DARKGRAY);
+}
+
 int main(int argc, char *argv[]) {
   time_t current_time = time(NULL);
   char *time_string = ctime(&current_time);
@@ -17,29 +35,8 @@ int main(int argc, char *argv[]) {
     BeginDrawing();
     ClearBackground(BLACK);
 
-    int count = 6;
-
-    Vector2 center = {WIDTH / 2, HEIGHT / 2};
-
-    float segment_width = 200;
-    float segment_thickness = 50;
-
-    Vector2 a, b, c, d, e, f;
-    a = (Vector2){center.x - segment_width / 2 - segment_thickness / 2,
-                  center.y};
-    b = (Vector2){center.x - segment_width / 2,
-                  center.y - segment_thickness / 2};
-    c = (Vector2){center.x - segment_width / 2,
-                  center.y + segment_thickness / 2};
-    d = (Vector2){center.x + segment_width / 2,
-                  center.y - segment_thickness / 2};
-    e = (Vector2){center.x + segment_width / 2,
-                  center.y + segment_thickness / 2};
-    f = (Vector2){center.x + segment_width / 2 + segment_thickness / 2,
-                  center.y};
-
-    Vector2 points[] = {a, b, c, d, e, f};
-    DrawTriangleStrip(points, count, SKYBLUE);
+    Vector2 center = {300, 200};
+    drawStrip(center);
 
     EndDrawing();
   }
