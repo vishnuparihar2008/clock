71911861be94f0dfc95f53594490000d6e8353ca
vishu <vishnuparihar.delhi5@gmail.com>
2026-06-28 09:06:55 +0530

    BEAUTIFUL DIGITS

--- main.c
+++ main.c
@@ -6,10 +6,10 @@
 #define WIDTH 900
 #define HEIGHT 600
 
-#define OFFSET 70
+#define OFFSET 30
 
-float segment_width = 200;
-float segment_thickness = 50;
+float segment_width = 50;
+float segment_thickness = 25;
 
 int digits[10][7] = {{1, 1, 1, 0, 1, 1, 1}, {0, 0, 1, 0, 0, 1, 0},
                      {1, 0, 1, 1, 1, 0, 1}, {1, 0, 1, 1, 0, 1, 1},
@@ -59,19 +59,19 @@ void drawDigit(Vector2 center, int digit) {
   int *digit_segment = &digits[digit][0];
 
   drawStrip((Vector2){center.x, center.y - segment_width - OFFSET},
-  true, digit_segment[0] ? RED : DARKGRAY);
+  true, digit_segment[0] ? WHITE : GRAY);
   drawStrip((Vector2){center.x - segment_width / 2 - OFFSET/2, center.y - segment_width / 2 - OFFSET / 2},
-  false, digit_segment[1] ? RED : DARKGRAY);
+  false, digit_segment[1] ? WHITE : GRAY);
   drawStrip((Vector2){center.x + segment_width / 2 + OFFSET/2, center.y - segment_width / 2 - OFFSET / 2},
-  false, digit_segment[2] ? RED : DARKGRAY);
+  false, digit_segment[2] ? WHITE : GRAY);
   drawStrip((Vector2){center.x, center.y},
-  true, digit_segment[3] ? RED : DARKGRAY);
+  true, digit_segment[3] ? WHITE : GRAY);
   drawStrip((Vector2){center.x - segment_width / 2 - OFFSET/2, center.y + segment_width / 2 + OFFSET/2},
-  false, digit_segment[4] ? RED : DARKGRAY);
+  false, digit_segment[4] ? WHITE : GRAY);
   drawStrip((Vector2){center.x + segment_width / 2 + OFFSET/2, center.y + segment_width / 2 + OFFSET/2},
-  false, digit_segment[5] ? RED : WHITE);
+  false, digit_segment[5] ? WHITE : GRAY);
   drawStrip((Vector2){center.x, center.y + segment_width + OFFSET},
-  true, digit_segment[6] ? RED : DARKGRAY);
+  true, digit_segment[6] ? WHITE : GRAY);
 }
 
 int main(int argc, char *argv[]) {
@@ -84,9 +84,14 @@ int main(int argc, char *argv[]) {
 
   while (!WindowShouldClose()) {
     BeginDrawing();
-    ClearBackground(BLACK);
+    ClearBackground(DARKGRAY);
 
-    drawDigit((Vector2){WIDTH / 2, HEIGHT / 2}, 0);
+    drawDigit((Vector2){90, HEIGHT / 2}, 0);
+    drawDigit((Vector2){200, HEIGHT / 2}, 1);
+    drawDigit((Vector2){390, HEIGHT / 2}, 2);
+    drawDigit((Vector2){500, HEIGHT / 2}, 3);
+    drawDigit((Vector2){690, HEIGHT / 2}, 4);
+    drawDigit((Vector2){800, HEIGHT / 2}, 5);
 
     EndDrawing();
   }
